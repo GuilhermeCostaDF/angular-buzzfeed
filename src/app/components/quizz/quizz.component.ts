0b9722c5be385755aa719419bffd179a3a162062
GuilhermeCostaDF <guidocostadf@gmail.com>
2022-11-16 10:45:23 -0300

    propriedades logicas do componente quizz

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -7,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuizzComponent implements OnInit {
 
+  title: string = 'Título do componente';
+
+  questions: any;
+  questionSelected: any;
+
+  questionIndex: number = 0;
+  questionMaxIndex: number = 0;
+
+  finished: boolean = false;
+
+  answers: string[] = [];
+  answerSelected: string ='';
+
   constructor() { }
 
   ngOnInit(): void {
